b0c2d5b5b6e9720e1383d2fcdc1d75e52759f844
ueyama1995 <shinji160829033205@gmail.com>
2021-12-24 11:17:00 +0000

    [Update] いいねの非同期とレイアウト調整　jsできるけどtempate error起きてる

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,14 +5,12 @@ def create
   review = Review.find_by(params[:review_id])
   like = current_user.likes.new(review_id: review.id)
   like.save
-  redirect_to review_path(review)
 end
 
   def destroy
     review = Review.find_by(params[:review_id])
     like = current_user.likes.find_by(review_id: review.id)
     like.destroy
-    redirect_to review_path(review)
   end
 
 end
